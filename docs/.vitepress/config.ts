a78c0ab20ad864f6f8ebefc7c159a6a7d91f040d
Sanjay Soundarajan <ssoundarajan@calmi2.org>
2026-05-05 17:47:34 -0700

    feat: update favicon

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -19,6 +19,52 @@ export default defineConfig({
     [
       "link",
       {
+        rel: "icon",
+        type: "image/png",
+        href: "/favicon-96x96.png",
+        sizes: "96x96",
+      },
+    ],
+    [
+      "link",
+      {
+        rel: "icon",
+        type: "image/svg+xml",
+        href: "/favicon.svg",
+      },
+    ],
+    [
+      "link",
+      {
+        rel: "shortcut icon",
+        href: "/favicon.ico",
+      },
+    ],
+    [
+      "link",
+      {
+        rel: "apple-touch-icon",
+        sizes: "180x180",
+        href: "/apple-touch-icon.png",
+      },
+    ],
+    [
+      "meta",
+      {
+        name: "apple-mobile-web-app-title",
+        content: "MyWebSite",
+      },
+    ],
+    [
+      "link",
+      {
+        rel: "manifest",
+        href: "/site.webmanifest",
+      },
+    ],
+    [
+      "link",
+      {
         rel: "stylesheet",
         href: "https://cdn.jsdelivr.net/npm/katex@0.16.11/dist/katex.min.css",
       },
